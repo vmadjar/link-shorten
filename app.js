aadf377c9f01cedaac069a14a58ea68726e3b654
vmadjar <vasya.madzhar@gmail.com>
2022-03-17 09:48:35 +0200

    port-settings

--- app.js
+++ app.js
@@ -2,14 +2,14 @@ const express  = require('express')
 const config = require('config')
 const path = require('path')
 const mongoose = require('mongoose')
-
 const app = express()
 
-app.use(express.json({extended: true}))
 
+app.use(express.json({extended: true}))
 app.use('/api/auth', require('./routes/auth.routes'))
 app.use('/api/link', require('./routes/link.routes'))
 app.use('/t', require("./routes/redirect.routes"))
+
 
 if(process.env.NODE_ENV === production) {
     app.use('/', express.static(path.join(__dirname, "client", "build")))
@@ -18,8 +18,7 @@ if(process.env.NODE_ENV === production) {
     })
 }
 
-
-const PORT = config.get('port') || 5000
+const PORT =  process.env.PORT  || 8080
 
 async  function start () {
     try {
